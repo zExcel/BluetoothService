f938918d25954167caf5d4e3ce3b9f2e03e42f31
Justin Clark <j0c1a16@homeoffice.wal-mart.com>
2023-02-20 16:42:26 -0600

    Use integer divison

--- src/python/scripts/ws2812-spi/snake.py
+++ src/python/scripts/ws2812-spi/snake.py
@@ -114,8 +114,8 @@ def gameLoop():
     game_over = False
     game_close = False
  
-    x1 = dis_width / 2
-    y1 = dis_height / 2
+    x1 = dis_width // 2
+    y1 = dis_height // 2
  
     x1_change = 0
     y1_change = 0
